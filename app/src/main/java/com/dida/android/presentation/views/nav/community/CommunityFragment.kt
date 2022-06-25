be24833b569ced65574d9a175de9f8d59d0136d7
SeoSeungHwan <tmdghks3264@naver.com>
2022-06-25 22:10:34 +0900

    feat. 커뮤니티 페이지 Community, Comment 예제 두개 추가

--- app/src/main/java/com/dida/android/presentation/views/nav/community/CommunityFragment.kt
+++ app/src/main/java/com/dida/android/presentation/views/nav/community/CommunityFragment.kt
@@ -6,9 +6,12 @@ import com.dida.android.R
 import com.dida.android.databinding.FragmentCommunityBinding
 import com.dida.android.domain.model.nav.community.ActiveNFTHolderModel
 import com.dida.android.domain.model.nav.community.ReservationNFTHolderModel
+import com.dida.android.domain.model.nav.detailnft.Comments
+import com.dida.android.domain.model.nav.detailnft.Community
 import com.dida.android.domain.model.nav.mypage.WalletNFTHistoryHolderModel
 import com.dida.android.presentation.adapter.community.ActiveNFTRecyclerViewAdapter
 import com.dida.android.presentation.adapter.community.ReservationNFTRecyclerViewAdapter
+import com.dida.android.presentation.adapter.detailnft.CommunityAdapter
 import com.dida.android.presentation.adapter.mypage.WalletNFTHistoryRecyclerViewAdapter
 import com.dida.android.presentation.base.BaseFragment
 import com.dida.android.presentation.viewmodel.nav.community.CommunityViewModel
@@ -77,5 +80,30 @@ class CommunityFragment : BaseFragment<FragmentCommunityBinding, CommunityViewMo
                 orientation = LinearLayoutManager.HORIZONTAL
             }
         }
+
+        val commentList = mutableListOf(
+            Comments("https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", "NFT 가 너무 이쁘네요~~~! 미아러ㅣㅏ어미라ㅓ"),
+            Comments("https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", "NFT 가 너무 이쁘네요~~~! 미아러ㅣㅏ어미라ㅓ")
+        )
+
+        binding.communityRecyclerView.apply {
+            adapter = CommunityAdapter()
+            layoutManager = LinearLayoutManager(requireContext()).apply {
+                orientation = LinearLayoutManager.VERTICAL
+            }
+        }
+        val adapter = binding.communityRecyclerView.adapter as CommunityAdapter
+        adapter.addItem(Community("https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2",
+            "test", false, "test", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ",
+            "https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", "NFT name here",
+            "https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", 1.65, commentList
+        ))
+        adapter.addItem(Community("https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2",
+            "test", false, "test", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ",
+            "https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", "NFT name here",
+            "https://movie-phinf.pstatic.net/20190417_250/1555465284425i6WQE_JPEG/movie_image.jpg?type=m665_443_2", 1.65, commentList
+        ))
+        adapter.notifyDataSetChanged()
+
     }
 }
