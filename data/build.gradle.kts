8413d78270f1f4e5a6b8f01a52035f2c4a0e7797
duru.hi <hwanggyuil@gmail.com>
2022-10-13 16:23:34 +0900

    fix. 데이터 레이어 version catalogs로 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -48,59 +48,46 @@ dependencies {
 
     implementation(project(mapOf("path" to ":domain")))
 
-    // GsonConverter & Retrofit2 & Okhttp3 For Network Connection
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.2")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    // https://github.com/square/okhttp/tree/master/okhttp-logging-interceptor
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation(libs.bundles.gson)
+    implementation(libs.squareup.retrofit2)
+    implementation(libs.squareup.okhttp)
+    implementation(libs.squareup.okhttp.interceptor)
 
     // Glide Library For Image
-    implementation("com.github.bumptech.glide:glide:4.13.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
+    implementation(libs.glide.glide)
+    annotationProcessor(libs.glide.compiler)
 
     //Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    // Testing Navigation
-    androidTestImplementation("androidx.navigation:navigation-testing:2.3.5")
+    implementation(libs.bundles.androidx.navigation)
+    androidTestImplementation(libs.androidx.navigation.test)
 
     // https://github.com/ybq/Android-SpinKit
-    implementation("com.github.ybq:Android-SpinKit:1.4.0")
+    implementation(libs.android.spinkit)
 
     // life cycle scope
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
-    // viewmodel scope
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
+    implementation(libs.bundles.lifecycle)
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.40.5")
-    kapt("com.google.dagger:hilt-compiler:2.40.5")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation(libs.kotlin.coroutine.core)
+    implementation(libs.kotlin.coroutine.android)
 
     //Room
-    api("androidx.room:room-runtime:2.4.1")
-    kapt("androidx.room:room-compiler:2.4.1")
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.4.1")
-
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.bundles.room)
+    
     // 카카오 로그인
-    implementation("com.kakao.sdk:v2-user:2.8.4")
+    implementation(libs.kakao.sdk)
 
     //Circle ImageView
-    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation(libs.circle.imageview)
 
     // moshi
-    implementation("com.squareup.moshi:moshi:1.9.3")
-    // for code-gen
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.9.3")
-    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation(libs.bundles.moshi)
 
     // Preferences Data Store
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-core:1.0.0")
+    implementation(libs.bundles.datastore)
 }
