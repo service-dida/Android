830d617d7cff92a56d9c02a44b5d3d2ad654806a
duru.hi <hwanggyuil@gmail.com>
2023-01-25 16:31:28 +0900

    [DIDA] feat. 로그인 > 로그인 관련 리소스 이전

--- presentation/src/main/java/com/dida/android/presentation/views/LoginMainFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/LoginMainFragment.kt
@@ -1,18 +1,16 @@
 package com.dida.android.presentation.views
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.dida.android.R
-import com.dida.android.databinding.FragmentLoginmainBinding
-import com.dida.android.presentation.activities.NavHostActivity
 import com.dida.android.util.toLoginFailure
 import com.dida.android.util.toLoginSuccess
 import com.dida.login.LoginMainViewModel
 import com.dida.login.LoginNavigationAction
+import com.dida.login.databinding.FragmentLoginmainBinding
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
@@ -20,12 +18,12 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewModel>(R.layout.fragment_loginmain) {
+class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewModel>(com.dida.login.R.layout.fragment_loginmain) {
 
     private val TAG = "LoginMainFragment"
 
     override val layoutResourceId: Int
-        get() = R.layout.fragment_loginmain // get() : 커스텀 접근자, 코틀린 문법
+        get() = com.dida.login.R.layout.fragment_loginmain // get() : 커스텀 접근자, 코틀린 문법
 
     override val viewModel : LoginMainViewModel by viewModels()
 
