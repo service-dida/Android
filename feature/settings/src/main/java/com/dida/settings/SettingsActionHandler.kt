a971bb0a26923b09b39d588e09fa8c483de84251
duru.hi <hwanggyuil@gmail.com>
2023-01-28 22:22:07 +0900

    [DIDA] 설정 > 설정 관련 Ui추가

---
+++ feature/settings/src/main/java/com/dida/settings/SettingsActionHandler.kt
@@ -0,0 +1,11 @@
+package com.dida.settings
+
+interface SettingsActionHandler {
+    fun onProfileEditClicked()
+    fun onPrePasswordClicked()
+    fun onPasswordEditClicked()
+    fun onAccountInformationClicked()
+    fun onNotificationClicked()
+    fun onInVisibleClicked()
+    fun onLogoutClicked()
+}
