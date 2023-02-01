d014ffd69feb3b46f6bdf0233c641cdbd0772be1
서승환 <tmdghks3264@naver.com>
2023-02-02 08:48:28 +0900

    [DIDA] feat. 스왑 > 스왑내역화면 HolderModel 구현

--- feature/swap-history/src/main/java/com/dida/swap/history/SwapHistoryViewModel.kt
+++ feature/swap-history/src/main/java/com/dida/swap/history/SwapHistoryViewModel.kt
@@ -1,7 +1,6 @@
 package com.dida.swap.history
 
 import com.dida.common.base.BaseViewModel
-import com.dida.common.util.UiState
 import com.dida.domain.model.nav.swap_history.SwapHistory
 import com.dida.domain.onError
 import com.dida.domain.onSuccess
@@ -16,17 +15,18 @@ import javax.inject.Inject
 @HiltViewModel
 class SwapHistoryViewModel @Inject constructor(
     private val swapHistoryAPI: SwapHistoryAPI
-) : BaseViewModel() {
+) : BaseViewModel() , SwapHistoryActionHandler{
 
     private val TAG = "SwapHistoryViewModel"
 
-    private val _swapHistoryState: MutableStateFlow<UiState<List<SwapHistory>>> = MutableStateFlow(UiState.Loading)
-    val swapHistoryState: StateFlow<UiState<List<SwapHistory>>> = _swapHistoryState.asStateFlow()
+    private val _swapHistoryState: MutableStateFlow<List<SwapHistory>> = MutableStateFlow<List<SwapHistory>>(emptyList())
+    val swapHistoryState: StateFlow<List<SwapHistory>> = _swapHistoryState.asStateFlow()
 
     fun getSwapHistory(){
         baseViewModelScope.launch {
             swapHistoryAPI()
-                .onSuccess { _swapHistoryState.value = UiState.Success(it) }
+                .onSuccess { _swapHistoryState.emit(it)
+                it.get(0).timeToDayAndTime()}
                 .onError { e -> catchError(e) }
         }
     }
--- presentation/src/main/java/com/dida/android/presentation/views/SwapHistoryFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/SwapHistoryFragment.kt
@@ -1,12 +1,16 @@
 package com.dida.android.presentation.views
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.dida.swap.history.R
+import com.dida.swap.history.adapter.SwapHistoryAdapter
 import com.dida.swap.history.SwapHistoryViewModel
 import com.dida.swap.history.databinding.FragmentSwapHistoryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SwapHistoryFragment : BaseFragment<FragmentSwapHistoryBinding, SwapHistoryViewModel>(R.layout.fragment_swap_history) {
@@ -20,6 +24,7 @@ class SwapHistoryFragment : BaseFragment<FragmentSwapHistoryBinding, SwapHistory
 
     private val navController: NavController by lazy { findNavController() }
 
+    private val swapHistoryAdapter by lazy { SwapHistoryAdapter(viewModel) }
     override fun initStartView() {
         binding.apply {
             this.vm = viewModel
@@ -27,11 +32,18 @@ class SwapHistoryFragment : BaseFragment<FragmentSwapHistoryBinding, SwapHistory
         }
         exception = viewModel.errorEvent
         initToolbar()
+        initAdapter()
         viewModel.getSwapHistory()
     }
 
     override fun initDataBinding() {
-
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            launch {
+                viewModel.swapHistoryState.collectLatest {
+                    swapHistoryAdapter.submitList(it)
+                }
+            }
+        }
     }
 
     override fun initAfterBinding() {
@@ -43,5 +55,9 @@ class SwapHistoryFragment : BaseFragment<FragmentSwapHistoryBinding, SwapHistory
             this.setNavigationIcon(com.dida.android.R.drawable.ic_back)
             this.setNavigationOnClickListener { navController.popBackStack() }
         }
+    }
+
+    private fun initAdapter(){
+        binding.swapHistoryRv.adapter = swapHistoryAdapter
     }
 }
