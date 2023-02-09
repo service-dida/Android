296ad5c26e86ac2022f9df95ab9f5e1d709eb2a6
서승환 <tmdghks3264@naver.com>
2023-02-09 14:51:42 +0900

    [DIDA] feat. NFT구매 > NFT 구매 모듈생성 및 의존성 추가

--- build-logic/convention/src/main/java/com/example/convention/AndroidPresentationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/example/convention/AndroidPresentationConventionPlugin.kt
@@ -60,6 +60,7 @@ class AndroidPresentationConventionPlugin : Plugin<Project> {
                     add("implementation", project(":feature:recent-nft"))
                     add("implementation", project(":feature:hot-user"))
                     add("implementation", project(":feature:hot-seller"))
+                    add("implementation", project(":feature:buy-nft"))
 
                     // Android Common
                     add("implementation", libs.findLibrary("androidx-core").get())
