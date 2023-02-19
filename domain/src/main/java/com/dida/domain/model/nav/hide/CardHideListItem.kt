6a66fdc9f66989bb0318d0122381e33b24eb80fa
서승환 <tmdghks3264@naver.com>
2023-02-19 23:03:41 +0900

    [DIDA] feat. NFT 숨김목록 > 유저이름 및 가격 추가

--- domain/src/main/java/com/dida/domain/model/nav/hide/CardHideListItem.kt
+++ domain/src/main/java/com/dida/domain/model/nav/hide/CardHideListItem.kt
@@ -3,5 +3,7 @@ package com.dida.domain.model.nav.hide
 data class CardHideList(
     val cardId: Int,
     val cardUrl: String,
-    val cardTitle: String
+    val cardTitle: String,
+    val userName : String,
+    val price : String
 )
