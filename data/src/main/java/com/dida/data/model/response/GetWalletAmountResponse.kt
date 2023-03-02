0816963ff8c24a12ff3056dcf617d54bf27a8755
서승환 <tmdghks3264@naver.com>
2023-03-02 16:35:29 +0900

    [DIDA] feat. 지갑 > 지갑 주소 표기 및 클립보드 복사하기 기능 추가

--- data/src/main/java/com/dida/data/model/response/GetWalletAmountResponse.kt
+++ data/src/main/java/com/dida/data/model/response/GetWalletAmountResponse.kt
@@ -3,6 +3,9 @@ package com.dida.data.model.response
 import com.google.gson.annotations.SerializedName
 
 data class GetWalletAmountResponse(
+    @SerializedName("walletId") val walletId: Long,
+    @SerializedName("address") val address: String,
     @SerializedName("dida") val dida: Double,
     @SerializedName("klay") val klay: Double
+
 )
