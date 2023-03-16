48eaf6e267a83a3b98c168c615f120aa05d98d55
서승환 <tmdghks3264@naver.com>
2023-03-16 12:38:11 +0900

    [DIDA] fix. 로그인 > BaseFragment 토스트에 필요한 context 를 requireContext로 수정

--- presentation/src/main/java/com/dida/android/presentation/views/BaseFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/BaseFragment.kt
@@ -181,13 +181,13 @@ abstract class BaseFragment<T : ViewDataBinding, R : BaseViewModel>(layoutId: In
     // Toast Message 관련 함수
     protected fun showToastMessage(e: Throwable?) {
         toast?.cancel()
-        toast = Toast.makeText(activity, e?.cause?.message ?: "알 수 없는 에러가 발생했습니다.", Toast.LENGTH_SHORT)?.apply { show() }
+        toast = Toast.makeText(requireContext(), e?.cause?.message ?: "알 수 없는 에러가 발생했습니다.", Toast.LENGTH_SHORT)?.apply { show() }
     }
 
     // Toast Message 관련 함수
     protected fun toastMessage(message: String) {
         toast?.cancel()
-        toast = Toast.makeText(activity, message, Toast.LENGTH_SHORT)?.apply { show() }
+        toast = Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT)?.apply { show() }
     }
 
     // navigation 중복체크 관리 <- checkNavigation 대신 사용할것
