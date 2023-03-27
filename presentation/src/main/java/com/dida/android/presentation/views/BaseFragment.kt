18bdf2157a7d4a5330c32b20f37f1c37a45c918a
duru.hi <hwanggyuil@gmail.com>
2023-03-27 20:35:12 +0900

    [DIDA] fix. Common > BaseFragment 기존 collect 부분 수정

--- presentation/src/main/java/com/dida/android/presentation/views/BaseFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/BaseFragment.kt
@@ -21,9 +21,7 @@ import com.dida.android.NavigationGraphDirections
 import com.dida.android.presentation.activities.LoginActivity
 import com.dida.common.base.BaseViewModel
 import com.dida.common.base.LoadingDialogFragment
-import com.dida.common.util.Scheme
-import com.dida.common.util.SchemeUtils
-import com.dida.common.util.repeatOnStarted
+import com.dida.common.util.*
 import com.dida.data.model.InternalServerErrorException
 import com.dida.data.model.ServerNotFoundException
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -89,30 +87,32 @@ abstract class BaseFragment<T : ViewDataBinding, R : BaseViewModel>(layoutId: In
 
     init {
         lifecycleScope.launch {
-            launch {
-                exception?.collectLatest { exception ->
-                    sendException(exception)
-                    showToastMessage(exception)
+            repeatOnResumed {
+                launch {
+                    exception?.collectLatest { exception ->
+                        sendException(exception)
+                        showToastMessage(exception)
+                    }
                 }
-            }
 
-            launch {
-                viewModel.errorEvent.collectLatest { e ->
-                    sendException(e)
-                    dismissLoadingDialog()
-                    showToastMessage(e)
+                launch {
+                    viewModel.errorEvent.collectLatest { e ->
+                        sendException(e)
+                        dismissLoadingDialog()
+                        showToastMessage(e)
+                    }
                 }
-            }
 
-            launch {
-                viewModel.loadingEvent.collectLatest {
-                    if(it) showLoadingDialog()
-                    else dismissLoadingDialog()
+                launch {
+                    viewModel.loadingEvent.collectLatest {
+                        if(it) showLoadingDialog()
+                        else dismissLoadingDialog()
+                    }
                 }
-            }
 
-            launch {
-                viewModel.needLoginEvent.collectLatest { loginCheck() }
+                launch {
+                    viewModel.needLoginEvent.collectLatest { loginCheck() }
+                }
             }
         }
     }
