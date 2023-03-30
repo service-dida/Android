11583a986157153fdeb23c4c1b3b1a86abb1cd59
서승환 <tmdghks3264@naver.com>
2023-03-30 11:21:31 +0900

    [DIDA] fix. Local.Properties > 매니페스트 KAKAO Key Secret 에러 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
