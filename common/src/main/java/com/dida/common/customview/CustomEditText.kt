793ac4bf6b0d33f411ab92f82b64184cc75ec8c7
duru.hi <hwanggyuil@gmail.com>
2023-04-21 16:53:33 +0900

    [DIDA] refactor. CustomView > Focus 관련 키보드 Visible 기능 추가

--- common/src/main/java/com/dida/common/customview/CustomEditText.kt
+++ common/src/main/java/com/dida/common/customview/CustomEditText.kt
@@ -2,28 +2,31 @@ package com.dida.common.customview
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
 import com.dida.common.R
+
 
 class CustomEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
-
     init {
         //포커스에 따라 테두리색 변경
         setOnFocusChangeListener { _, isFocus ->
             if (isFocus) {
                 setBackgroundResource(R.drawable.custom_brandlemon_radius10_surface5_width1)
+                showKeyBoard()
             } else {
                 setBackgroundResource(R.drawable.custom_surface2_radius10_surface5_width1)
+                hideKeyBoard()
             }
         }
 
@@ -45,5 +48,15 @@ class CustomEditText @JvmOverloads constructor(
                 }
             })
         }
+    }
+
+    private fun showKeyBoard() {
+        val inputMethodManager = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, 0)
+    }
+
+    private fun hideKeyBoard() {
+        val inputMethodManager = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
     }
 }
