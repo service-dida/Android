09717d6f91c4b532125e86f4df9052b23dde27d0
duru.hi <hwanggyuil@gmail.com>
2023-05-11 22:54:52 +0900

    [DIDA] fix. 신고 > 신고 관련 Exception 수정

--- common/src/main/java/com/dida/common/base/BaseViewModel.kt
+++ common/src/main/java/com/dida/common/base/BaseViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dida.data.DataApplication
 import com.dida.data.model.HaveNotJwtTokenException
-import com.dida.data.model.InvalidJwtTokenException
 import com.dida.data.model.InvalidKakaoAccessTokenException
 import com.dida.data.model.NeedLogin
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -32,14 +31,13 @@ abstract class BaseViewModel : ViewModel() {
     private val _needLoginEvent: MutableSharedFlow<Boolean> = MutableSharedFlow<Boolean>()
     val needLoginEvent: SharedFlow<Boolean> = _needLoginEvent
 
-    suspend fun catchError(e: Throwable?) {
-        e?.let {
-            when(it) {
-                is HaveNotJwtTokenException, is InvalidKakaoAccessTokenException, is NeedLogin -> {
-                    DataApplication.dataStorePreferences.removeAccountToken()
-                    _needLoginEvent.emit(true)
-                }
+    protected suspend fun catchError(exception: Throwable) {
+        when(exception) {
+            is HaveNotJwtTokenException, is InvalidKakaoAccessTokenException, is NeedLogin -> {
+                DataApplication.dataStorePreferences.removeAccountToken()
+                _needLoginEvent.emit(true)
             }
+            else -> _errorEvent.emit(exception)
         }
     }
 
