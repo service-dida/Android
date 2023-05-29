7b936fce9ee2949d77ef81dc13cc1a215f01c259
duru.hi <hwanggyuil@gmail.com>
2023-05-29 23:04:39 +0900

    [DIDA] feat. Compose > 컴포즈 세팅 추가

--- feature/compose/src/main/java/com/dida/compose/theme/Theme.kt
+++ feature/compose/src/main/java/com/dida/compose/theme/Theme.kt
@@ -1,6 +1,9 @@
 package com.dida.compose.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
@@ -8,6 +11,27 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
 
 val Pretendard = androidx.compose.ui.text.font.FontFamily(
     Font(com.dida.common.R.font.pretendard_bold, FontWeight.Bold),
@@ -58,9 +82,28 @@ val DidaTypography = Typography (
         fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-
+    ),
 )
+
+@Composable
+fun DIDA_THEME(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
 
 @Composable
 fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
