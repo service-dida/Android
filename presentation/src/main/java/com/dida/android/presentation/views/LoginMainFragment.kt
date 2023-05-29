d66a9d491dd034597c0e97f7e24ddc8877685fed
duru.hi <hwanggyuil@gmail.com>
2023-05-29 23:05:17 +0900

    [DIDA] refactor. 로그인 > Compose View로 대체

--- presentation/src/main/java/com/dida/android/presentation/views/LoginMainFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/LoginMainFragment.kt
@@ -2,10 +2,10 @@ package com.dida.android.presentation.views
 
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,22 +13,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import coil.compose.AsyncImage
 import com.dida.android.R
 import com.dida.android.util.toLoginFailure
 import com.dida.android.util.toLoginSuccess
+import com.dida.compose.theme.DIDA_THEME
+import com.dida.compose.theme.DidaTypography
 import com.dida.login.LoginMainViewModel
 import com.dida.login.LoginNavigationAction
 import com.dida.login.databinding.FragmentLoginmainBinding
@@ -40,14 +46,15 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewModel>(com.dida.login.R.layout.fragment_loginmain) {
+class LoginMainFragment :
+    BaseFragment<FragmentLoginmainBinding, LoginMainViewModel>(com.dida.login.R.layout.fragment_loginmain) {
 
     private val TAG = "LoginMainFragment"
 
     override val layoutResourceId: Int
         get() = com.dida.login.R.layout.fragment_loginmain // get() : 커스텀 접근자, 코틀린 문법
 
-    override val viewModel : LoginMainViewModel by viewModels()
+    override val viewModel: LoginMainViewModel by viewModels()
 
     override fun initStartView() {
         binding.apply {
@@ -63,29 +70,31 @@ class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewMo
                 when (it) {
                     is LoginNavigationAction.NavigateToNickname -> {
                         showToastMessage("회원가입이 필요합니다.")
-                        navigate(LoginMainFragmentDirections.actionLoginMainFragmentToNicknameFragment(it.email))
+                        navigate(
+                            LoginMainFragmentDirections.actionLoginMainFragmentToNicknameFragment(
+                                it.email
+                            )
+                        )
                     }
+
                     is LoginNavigationAction.NavigateToHome -> {
                         showToastMessage("로그인에 성공하였습니다.")
                         this@LoginMainFragment.toLoginSuccess()
                     }
-                    is LoginNavigationAction.NavigateToLogin -> kakaoLogin()
+
+                    is LoginNavigationAction.NavigateToLogin -> onKakaoLogin()
                 }
             }
         }
     }
 
-    override fun initAfterBinding() {
-        binding.closeBtn.setOnClickListener {
-            this@LoginMainFragment.toLoginFailure()
-        }
-    }
+    override fun initAfterBinding() {}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //TODO: back key 이벤트 시 필요한 코드 추가
         view.setOnKeyListener { _, keyCode, event ->
-            if(keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.KEYCODE_BACK) {
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.KEYCODE_BACK) {
                 this@LoginMainFragment.toLoginFailure()
                 return@setOnKeyListener true
             }
@@ -95,14 +104,15 @@ class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewMo
         binding.composeView.apply {
             setContent {
                 LoginScreen(
-                    onKakaoLoginClicked = { },
-                    onKakakoWebLoginClicked = {}
+                    onKakaoLoginClicked = { onKakaoLogin() },
+                    onKakakoWebLoginClicked = { onKakaoLogin() },
+                    onCloseButtonClicked = { this@LoginMainFragment.toLoginFailure() }
                 )
             }
         }
     }
 
-    private fun kakaoLogin() {
+    private fun onKakaoLogin() {
         val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 when (error.toString()) {
@@ -122,68 +132,118 @@ class LoginMainFragment : BaseFragment<FragmentLoginmainBinding, LoginMainViewMo
         }
 
         // 카카오톡 설치여부 확인
-        if (viewModel.kakaoTalkLoginState.value && UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+        if (viewModel.kakaoTalkLoginState.value && UserApiClient.instance.isKakaoTalkLoginAvailable(
+                requireContext()
+            )
+        ) {
             UserApiClient.instance.loginWithKakaoTalk(requireContext(), callback = kakaoCallback)
         } else {
             UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = kakaoCallback)
         }
     }
+}
 
-    @Composable
-    fun LoginScreen(onKakaoLoginClicked: () -> Unit, onKakakoWebLoginClicked: () -> Unit) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                LogoImage()
-                Spacer(modifier = Modifier.weight(1f))
-                KakaoTalkButton(onKakaoLoginClicked)
-                KakaoWebButton(onKakakoWebLoginClicked)
-            }
+@Composable
+fun LoginScreen(
+    onKakaoLoginClicked: () -> Unit,
+    onKakakoWebLoginClicked: () -> Unit,
+    onCloseButtonClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color(0xFF121212))
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp)
+                .clickable { onCloseButtonClicked() },
+            painter = painterResource(id = com.dida.common.R.drawable.ic_close_white),
+            contentDescription = "닫기 버튼"
+        )
+        Spacer(modifier = Modifier.fillMaxWidth().height(187.dp))
+        Image(
+            modifier = Modifier
+                .size(132.dp)
+                .align(Alignment.CenterHorizontally),
+            painter = painterResource(id = com.dida.common.R.mipmap.img_dida_logo_foreground),
+            contentDescription = "디다 로고"
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "빠르게 로그인하기",
+            style = DidaTypography.caption,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.fillMaxWidth().height(16.dp))
+        onKakaoLoginButton(onKakaoLoginClicked)
+        Spacer(modifier = Modifier.fillMaxWidth().height(16.dp))
+        onKakaoWebLoginButton(onKakakoWebLoginClicked)
+        Spacer(modifier = Modifier.fillMaxWidth().height(32.dp))
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun onKakaoLoginButton(onKakaoLoginClicked: () -> Unit) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        color = Color(0xFFFEE500),
+        shape = RoundedCornerShape(8.dp),
+        onClick = { onKakaoLoginClicked() }
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Image(painter = painterResource(id = R.drawable.kakao_logo_1), contentDescription = "카카오 로고")
+            Spacer(modifier = Modifier.size(14.dp))
+            Text(
+                text = "카카오톡으로 시작하기",
+                style = DidaTypography.h3,
+                fontSize = 15.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
+}
 
-    @Composable
-    fun LogoImage() {
-        AsyncImage(
-                modifier = Modifier.width(128.dp).height(128.dp),
-                model = painterResource(id = com.dida.common.R.mipmap.img_dida_logo_foreground),
-                contentDescription = null,
-                contentScale = ContentScale.Crop
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun onKakaoWebLoginButton(onKakakoWebLoginClicked: () -> Unit) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 16.dp),
+        color = Color(0xFF121212),
+        shape = RoundedCornerShape(8.dp),
+        onClick = { onKakakoWebLoginClicked() }
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
+            text = "카카오계정 직접 입력하기",
+            style = DidaTypography.h3,
+            fontSize = 15.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center
         )
     }
+}
 
-    @Composable
-    fun KakaoTalkButton(onKakaoLoginClicked: () -> Unit) {
-        Button(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            onClick = onKakaoLoginClicked
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(16.dp),
-            ) {
-                AsyncImage(
-                    modifier = Modifier.wrapContentSize(),
-                    model = painterResource(id = R.drawable.kakao_logo_1),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
-                Text(
-                    modifier = Modifier.fillMaxWidth().padding(16.dp),
-                    text = requireContext().getString(R.string.kakao_login),
-                )
-            }
-        }
-    }
 
-    @Composable
-    fun KakaoWebButton(onKakakoWebLoginClicked: () -> Unit) {
-        Button(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            onClick = onKakakoWebLoginClicked
-        ) {
-            Text(
-                text = requireContext().getString(R.string.kakao_login_web),
-                fontSize = 15.sp,
-            )
-        }
+@Preview
+@Composable
+fun LoginPreview() {
+    DIDA_THEME {
+        LoginScreen(
+            onKakaoLoginClicked = {},
+            onKakakoWebLoginClicked = {},
+            onCloseButtonClicked = {}
+        )
     }
 }
