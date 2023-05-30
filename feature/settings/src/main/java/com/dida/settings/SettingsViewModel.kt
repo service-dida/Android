6bb25cf2ba5e3a6553210cc9c46a192ad7a6871a
Gyuil-Hwang <hwanggyuil@gmail.com>
2023-05-31 00:26:39 +0900

    [DIDA] migration. 설정 > 설정 화면 Compose View로 수정

--- feature/settings/src/main/java/com/dida/settings/SettingsViewModel.kt
+++ feature/settings/src/main/java/com/dida/settings/SettingsViewModel.kt
@@ -9,53 +9,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : BaseViewModel(), SettingsActionHandler {
+class SettingsViewModel @Inject constructor() : BaseViewModel() {
 
     private val TAG = "SettingsViewModel"
 
-    private val _navigationEvent: MutableSharedFlow<SettingsNavigationAction> = MutableSharedFlow<SettingsNavigationAction>()
-    val navigationEvent: SharedFlow<SettingsNavigationAction> = _navigationEvent
-
-    override fun onProfileEditClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToProfileEdit)
-        }
-    }
-
-    override fun onPasswordEditClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToPasswordEdit)
-        }
-    }
-
-    override fun onAccountInformationClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToAccountInformation)
-        }
-    }
-
-    override fun onNotificationClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToNotification)
-        }
-    }
-
-    override fun onInVisibleClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToHideList)
-        }
-    }
-
-    override fun onLogoutClicked() {
-        baseViewModelScope.launch {
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToLogout)
-        }
-    }
+    private val _navigateToMainEvent: MutableSharedFlow<Unit> = MutableSharedFlow<Unit>()
+    val navigateToMainEvent: SharedFlow<Unit> = _navigateToMainEvent
 
     fun logOut() {
         baseViewModelScope.launch {
             DataApplication.dataStorePreferences.removeAccountToken()
-            _navigationEvent.emit(SettingsNavigationAction.NavigateToLogoutComplete)
+            _navigateToMainEvent.emit(Unit)
         }
     }
 }
