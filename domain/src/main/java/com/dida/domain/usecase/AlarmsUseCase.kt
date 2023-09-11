c4071b4b617f5feb3b9f0b0fad1a2c27f85ad0d8
duru.hi <hwanggyuil@gmail.com>
2023-09-11 19:09:45 +0900

    [DIDA] 알림 목록 API

--- domain/src/main/java/com/dida/domain/usecase/AlarmsUseCase.kt
+++ domain/src/main/java/com/dida/domain/usecase/AlarmsUseCase.kt
@@ -1,13 +1,15 @@
 package com.dida.domain.usecase
 
+import com.dida.domain.Contents
 import com.dida.domain.NetworkResult
 import com.dida.domain.main.MainRepository
+import com.dida.domain.main.model.Alarm
 import javax.inject.Inject
 
-class CancelBlockUseCase @Inject constructor(
+class AlarmsUseCase @Inject constructor(
     private val repository: MainRepository
 ) {
-    suspend operator fun invoke(postId: Long) : NetworkResult<Unit> {
-        return repository.blockPost(postId)
+    suspend operator fun invoke(page: Int, size: Int) : NetworkResult<Contents<Alarm>> {
+        return repository.alarms(page, size)
     }
 }
