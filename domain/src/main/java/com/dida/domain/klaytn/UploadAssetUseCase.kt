381ace1b9177d7f9244aa703113dbe90cd5599c1
duru.hi <hwanggyuil@gmail.com>
2023-09-15 20:49:35 +0900

    [DIDA] UploadAssets API 클래스 수정

--- domain/src/main/java/com/dida/domain/klaytn/UploadAssetUseCase.kt
+++ domain/src/main/java/com/dida/domain/klaytn/UploadAssetUseCase.kt
@@ -1,12 +1,10 @@
 package com.dida.domain.klaytn
 
 import com.dida.domain.NetworkResult
-import com.dida.domain.klaytn.Asset
-import com.dida.domain.klaytn.KlaytnRepository
 import okhttp3.MultipartBody
 import javax.inject.Inject
 
-class UploadAssetAPI @Inject constructor(
+class UploadAssetUseCase @Inject constructor(
     private val repository: KlaytnRepository
 ){
     suspend operator fun invoke(file: MultipartBody.Part) : NetworkResult<Asset> {
