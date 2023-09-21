4be4cd6fca3a98a4c7a316a599d044d8ad54a1a6
duru.hi <hwanggyuil@gmail.com>
2023-09-21 18:22:20 +0900

    [DIDA] 유저 프로필 > 타 유저 팔로잉 목록 보기 이동 액션 제거

--- feature/user-profile/src/main/java/com/dida/user_profile/UserProfileNavigationAction.kt
+++ feature/user-profile/src/main/java/com/dida/user_profile/UserProfileNavigationAction.kt
@@ -1,10 +1,6 @@
 package com.dida.user_profile
 
-import com.dida.domain.main.model.Follow
-import com.dida.domain.main.model.Nft
-
 sealed class UserProfileNavigationAction {
     class NavigateToDetailNft(val cardId: Long) : UserProfileNavigationAction()
-    class NavigateToUserFollowed(val userId: Long, val type: Follow): UserProfileNavigationAction()
     class NavigateToNftUpdate(val nftId: Long): UserProfileNavigationAction()
 }
