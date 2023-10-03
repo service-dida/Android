c278bc2b4a97eb35e7698c386e1030842a1c0ddb
duru.hi <hwanggyuil@gmail.com>
2023-10-03 19:51:11 +0900

    [DIDA] 활발한 활동 > 유저 팔로우 버튼 컬러 수정

--- feature/hot-user/src/main/java/com/dida/hot_user/HotUserBindingAdapters.kt
+++ feature/hot-user/src/main/java/com/dida/hot_user/HotUserBindingAdapters.kt
@@ -11,14 +11,13 @@ fun TextView.bindCardCount(count: Int) {
     view.text = "$count 작품"
 }
 
-@RequiresApi(Build.VERSION_CODES.M)
 @BindingAdapter("hotUserFollowBtn")
 fun TextView.bindHotUserFollowBtn(isFollow: Boolean) {
     val view = this
     if(isFollow) {
         view.text = context.getString(com.dida.common.R.string.user_following)
-        view.setTextColor(context.getColor(com.dida.common.R.color.white))
-        view.setBackgroundResource(com.dida.common.R.drawable.custom_surface6_radius100)
+        view.setTextColor(context.getColor(com.dida.common.R.color.black))
+        view.setBackgroundResource(com.dida.common.R.drawable.custom_brandlemon_radius100)
     } else {
         view.text = context.getString(com.dida.common.R.string.user_follow)
         view.setTextColor(context.getColor(com.dida.common.R.color.brand_lemon))
