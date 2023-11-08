b780a6de3d33e2b44169855a7e91383f82cb78f3
duru.hi <hwanggyuil@gmail.com>
2023-11-08 17:22:48 +0900

    [DIDA] NFT 상세 > 애니메이션 관련 코드 수정

--- common/src/main/java/com/dida/common/util/Animation.kt
+++ common/src/main/java/com/dida/common/util/Animation.kt
@@ -4,42 +4,63 @@ import android.annotation.SuppressLint
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationSet
-import android.view.animation.CycleInterpolator
 import android.view.animation.RotateAnimation
 import android.view.animation.ScaleAnimation
 import android.widget.ImageView
-import kotlinx.coroutines.delay
+
+private const val ScaleFrom: Float = 0f
+private const val ScaleTo: Float = 1.2f
+private const val ScaleDuration: Long = 200
+
+private const val RotateFrom: Float = -20f
+private const val RotateTo: Float = 20f
+private const val RotateDuration: Long = 300
+
+private const val PivotValue: Float = 0.5f
 
 @SuppressLint("UseCompatLoadingForDrawables")
-suspend fun ImageView.likeAnimation(
+fun ImageView.likeAnimation(
     like: Boolean
 ) {
     val imageSrc = if (like) context.getDrawable(com.dida.common.R.drawable.ic_empty_heart) else context.getDrawable(com.dida.common.R.drawable.ic_fill_heart)
     val imageView = this
     imageView.setImageDrawable(imageSrc)
+
+    val animationListener = object : Animation.AnimationListener {
+        override fun onAnimationStart(animation: Animation?) {
+            imageView.post { imageView.visibility = View.VISIBLE }
+        }
+        override fun onAnimationEnd(animation: Animation?) {
+            imageView.postDelayed({ imageView.visibility = View.GONE }, 100)
+        }
+        override fun onAnimationRepeat(animation: Animation?) = Unit
+    }
+
     val animSet = AnimationSet(true)
+
     val scaleAnimation = ScaleAnimation(
-        0f, 1.2f, 0f, 1.2f,
-        ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
-        ScaleAnimation.RELATIVE_TO_SELF, 0.5f
+        ScaleFrom, ScaleTo, ScaleFrom, ScaleTo,
+        ScaleAnimation.RELATIVE_TO_SELF, PivotValue,
+        ScaleAnimation.RELATIVE_TO_SELF, PivotValue
     ).apply {
-        duration = 200
+        duration = ScaleDuration
     }
 
     val rotationAnimation = RotateAnimation(
-        -20f, 20f, Animation.RELATIVE_TO_SELF,
-        0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+        RotateFrom, RotateTo,
+        Animation.RELATIVE_TO_SELF, PivotValue,
+        Animation.RELATIVE_TO_SELF, PivotValue
     ).apply {
-        duration = 300
-        startOffset = 50
+        duration = RotateDuration
         repeatMode = Animation.REVERSE
-        interpolator = CycleInterpolator(10f)
     }
-    animSet.addAnimation(scaleAnimation)
-    animSet.addAnimation(rotationAnimation)
 
-    imageView.visibility = View.VISIBLE
+    animSet.apply {
+        addAnimation(scaleAnimation)
+        addAnimation(rotationAnimation)
+        setAnimationListener(animationListener)
+    }
+
     imageView.startAnimation(animSet)
-    delay(500)
-    imageView.visibility = View.GONE
+
 }
