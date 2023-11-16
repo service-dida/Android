4dc012fc49ed5ddcf3c77372d7ee3a0fc9f3d806
duru.hi <hwanggyuil@gmail.com>
2023-11-16 21:57:58 +0900

    [DIDA] AI키워드 그리기 > 유저 키워드 입력 그리기

--- presentation/src/main/java/com/dida/android/presentation/views/KeywordInputFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/KeywordInputFragment.kt
@@ -1,10 +1,17 @@
 package com.dida.android.presentation.views
 
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.dida.ai.databinding.FragmentKeywordInputBinding
+import com.dida.ai.keyword.Keyword
+import com.dida.ai.keyword.KeywordType
+import com.dida.ai.keyword.KeywordViewModel
 import com.dida.ai.keyword.input.KeywordInputViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class KeywordInputFragment :
@@ -16,6 +23,7 @@ class KeywordInputFragment :
         get() = com.dida.ai.R.layout.fragment_keyword_input // get() : 커스텀 접근자, 코틀린 문법
 
     override val viewModel: KeywordInputViewModel by viewModels()
+    private val sharedViewModel: KeywordViewModel by activityViewModels()
     private val navController by lazy { findNavController() }
 
     override fun initStartView() {
@@ -26,7 +34,14 @@ class KeywordInputFragment :
         initToolbar()
     }
 
-    override fun initDataBinding() {}
+    override fun initDataBinding() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.navigationAction.collectLatest {
+                sharedViewModel.insertKeyword(type = KeywordType.Product, keyword = Keyword.Default(viewModel.userInput.value))
+                navigate(KeywordInputFragmentDirections.actionKeywordInputFragmentToKeywordResultFragment())
+            }
+        }
+    }
 
     override fun initAfterBinding() {}
 
