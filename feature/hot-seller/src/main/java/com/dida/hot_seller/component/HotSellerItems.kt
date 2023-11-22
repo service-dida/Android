a7458071ef7ec85512c5a4dc9a7ec52467156578
duru.hi <hwanggyuil@gmail.com>
2023-11-22 16:57:12 +0900

    [DIDA] 핫셀러 > 핫셀러 더보기 Ui 추가

--- feature/hot-seller/src/main/java/com/dida/hot_seller/component/HotSellerItems.kt
+++ feature/hot-seller/src/main/java/com/dida/hot_seller/component/HotSellerItems.kt
@@ -1,2 +1,135 @@
 package com.dida.hot_seller.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.dida.compose.theme.BrandLemon
+import com.dida.compose.theme.DidaTypography
+import com.dida.compose.theme.MainBlack
+import com.dida.compose.theme.Surface1
+import com.dida.compose.theme.White
+import com.dida.compose.theme.dpToSp
+import com.dida.compose.utils.DidaImage
+import com.dida.compose.utils.HorizontalDivider
+import com.dida.compose.utils.VerticalDivider
+import com.dida.compose.utils.clickableSingle
+import com.dida.domain.main.model.HotSellerPage
+
+@Composable
+fun HotSellerItem(
+    item: HotSellerPage,
+    onFollowButtonClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            DidaImage(
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(RoundedCornerShape(100.dp)),
+                model = item.memberInfo.profileUrl
+            )
+            HorizontalDivider(dp = 12)
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = item.memberInfo.memberName,
+                    style = DidaTypography.h3,
+                    fontSize = dpToSp(dp = 18.dp),
+                    color = White
+                )
+                VerticalDivider(dp = 4)
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "NFT ${item.memberInfo.nftCount}",
+                    style = DidaTypography.caption,
+                    fontSize = dpToSp(dp = 14.dp),
+                    color = White,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            if (item.memberInfo.followed) {
+                Surface(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = BrandLemon,
+                            shape = RoundedCornerShape(size = 100.dp)
+                        )
+                        .background(color = BrandLemon, shape = RoundedCornerShape(size = 100.dp))
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                        .clickableSingle { onFollowButtonClicked() },
+                    color = BrandLemon
+                ) {
+                    Text(
+                        text = "팔로잉",
+                        style = DidaTypography.body1,
+                        fontSize = dpToSp(dp = 12.dp),
+                        color = MainBlack
+                    )
+                }
+            } else {
+                Surface(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = BrandLemon,
+                            shape = RoundedCornerShape(size = 100.dp)
+                        )
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                        .clickableSingle { onFollowButtonClicked() },
+                    color = Surface1
+                ) {
+                    Text(
+                        text = "팔로우",
+                        style = DidaTypography.body1,
+                        fontSize = dpToSp(dp = 12.dp),
+                        color = White
+                    )
+                }
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding()
+                .clip(RoundedCornerShape(12)),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            item.nftImgUrl.forEach {
+                DidaImage(
+                    modifier = Modifier.weight(1f).aspectRatio(1f),
+                    model = it
+                )
+            }
+        }
+    }
+}
--- presentation/src/main/java/com/dida/android/presentation/views/HotSellerFragment.kt
+++ presentation/src/main/java/com/dida/android/presentation/views/HotSellerFragment.kt
@@ -1,14 +1,35 @@
 package com.dida.android.presentation.views
 
+import android.os.Bundle
+import android.view.View
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.fragment.findNavController
 import com.dida.android.R
+import com.dida.compose.theme.Surface2
+import com.dida.compose.utils.LineDivider
+import com.dida.compose.utils.VerticalDivider
+import com.dida.domain.main.model.Follow
 import com.dida.hot_seller.HotSellerViewModel
+import com.dida.hot_seller.component.HotSellerItem
 import com.dida.hot_seller.databinding.FragmentHotSellerBinding
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-// TODO : HotSeller 더보기 Ui 완성하기
 @AndroidEntryPoint
 class HotSellerFragment : BaseFragment<FragmentHotSellerBinding, HotSellerViewModel>(com.dida.hot_seller.R.layout.fragment_hot_seller) {
 
@@ -28,13 +49,33 @@ class HotSellerFragment : BaseFragment<FragmentHotSellerBinding, HotSellerViewMo
         initToolbar()
     }
 
-    override fun initDataBinding() {
+    override fun initDataBinding() {}
+
+    override fun initAfterBinding() {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val hotSellers by viewModel.hotSellerState.collectAsStateWithLifecycle()
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    item { VerticalDivider(dp = 19) }
+                    items(hotSellers.size) {
+                        HotSellerItem(
+                            item = hotSellers.content[it],
+                            onFollowButtonClicked = {}
+                        )
+                    }
+                    item { VerticalDivider(dp = 19) }
+                }
+            }
+        }
     }
 
-    override fun initAfterBinding() {
-    }
-
-    private fun initToolbar(){
+    private fun initToolbar() {
         binding.toolbar.apply {
             this.title = resources.getString(R.string.hot_seller)
             this.setTitleTextColor(ContextCompat.getColor(requireContext(), R.color.white))
