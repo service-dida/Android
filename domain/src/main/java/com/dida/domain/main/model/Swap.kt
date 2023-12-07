72dccf80199f58259b74c2f4f6db7d82896c6149
duru.hi <hwanggyuil@gmail.com>
2023-12-07 21:52:14 +0900

    [DIDA] 스왑 > 스왑 이미지 및 텍스트 Response에 맞게 수정

--- domain/src/main/java/com/dida/domain/main/model/Swap.kt
+++ domain/src/main/java/com/dida/domain/main/model/Swap.kt
@@ -1,6 +1,5 @@
 package com.dida.domain.main.model
 
-
 data class Swap(
     val transactionId: Long,
     val type: String,
@@ -8,6 +7,15 @@ data class Swap(
     val time: String,
 ) {
     fun getAmountUnit(): String {
-        return if(type == "K") coin.toString()+"klay" else coin.toString()+"dida"
+        return if(type == "SWAP1") coin.toString()+"dida" else coin.toString()+"klay"
     }
+
+    fun getSwapType(): SwapType {
+        val response = this.type
+        return if (response == "SWAP1") SwapType.KLAY_TO_DIDA else SwapType.DIDA_TO_KLAY
+    }
+}
+
+enum class SwapType {
+    KLAY_TO_DIDA, DIDA_TO_KLAY
 }
