d447768c3bd068a49dcae1c0cd8ea66f66f1f40b
duru.hi <hwanggyuil@gmail.com>
2023-12-18 22:26:50 +0900

    [DIDA] Compose > DidaText 추가

---
+++ feature/compose/src/main/java/com/dida/compose/common/DidaText.kt
@@ -0,0 +1,121 @@
+package com.dida.compose.common
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.dida.compose.theme.DidaTypography
+import com.dida.compose.theme.MainBlack
+import com.dida.compose.theme.dpToSp
+
+val DidaBoldTypography = DidaTypography.h3
+val DidaMediumTypography = DidaTypography.body1
+val DidaRegularTypography = DidaTypography.caption
+
+@Composable
+fun DidaBoldText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int = 22,
+    lineHeight: Int = fontSize + 4,
+    color: Color = MainBlack,
+    textAlign: TextAlign = TextAlign.Start,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: (TextLayoutResult) -> Unit = {}
+) {
+    Text(
+        modifier = modifier,
+        style = DidaBoldTypography,
+        text = text,
+        textAlign = textAlign,
+        fontSize = dpToSp(dp = fontSize.dp),
+        lineHeight = dpToSp(dp = lineHeight.dp),
+        color = color,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout
+    )
+}
+
+@Composable
+fun HairshopMediumText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int = 15,
+    lineHeight: Int = fontSize + 4,
+    color: Color = MainBlack,
+    textAlign: TextAlign = TextAlign.Start,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: (TextLayoutResult) -> Unit = {}
+) {
+    Text(
+        modifier = modifier,
+        style = DidaMediumTypography,
+        text = text,
+        textAlign = textAlign,
+        fontSize = dpToSp(dp = fontSize.dp),
+        lineHeight = dpToSp(dp = lineHeight.dp),
+        color = color,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout
+    )
+}
+
+@Composable
+fun HairshopRegularText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int = 14,
+    lineHeight: Int = fontSize + 4,
+    color: Color = MainBlack,
+    textAlign: TextAlign = TextAlign.Start,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: (TextLayoutResult) -> Unit = {}
+) {
+    Text(
+        modifier = modifier,
+        style = DidaRegularTypography,
+        text = text,
+        textAlign = textAlign,
+        fontSize = dpToSp(dp = fontSize.dp),
+        lineHeight = dpToSp(dp = lineHeight.dp),
+        color = color,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout
+    )
+}
